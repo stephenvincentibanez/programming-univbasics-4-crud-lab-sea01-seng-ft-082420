8b9b6677ddc4ee50f92537d801de20f813dd6fa6
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-03 19:16:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,28 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["John Lennon", 
+  "Paul McCartney", 
+  "George Harrison", 
+  "Ringo Starr"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
